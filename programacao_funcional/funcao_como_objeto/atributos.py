59bbfb8dc54e21b47ac28d61be0c40467bc23cf2
Paulo Santos <paulo.santos@buser.com.br>
2023-01-17 10:05:10 -0300

    - Observações sobre lambda

--- programacao_funcional/funcao_como_objeto/atributos.py
+++ programacao_funcional/funcao_como_objeto/atributos.py
@@ -1,5 +1,8 @@
 def dobro(x):
-	return x*2
+	return x * 2
+
+
+dobro2 = lambda x: x * 2
 
 # se abrirmos o python console e executarmos
 # for p in dir(dobro):
@@ -15,7 +18,7 @@ def dobro(x):
 
 # os objetos são dinâmicos em python.
 # se tentarmos acessar o atributo n da função dobro (dobro.n), vamos tomar um AttributeError, pois ele não existe.
-#Podemos então, CRIAR esse atributo, atribuindo um valor a ele (dobro.n=42, por exemplo). A partir desse momento,
+# Podemos então, CRIAR esse atributo, atribuindo um valor a ele (dobro.n=42, por exemplo). A partir desse momento,
 # ele estará acessível.
 # Podemos também deletar esse atributo (del dobro.n)
 
@@ -23,3 +26,11 @@ def dobro(x):
 # ao fazer g = dobro (sem os parênteses, pois não estamos executando a função) e executar g no console, teremos como
 # retorno <function dobro at ...>, mostrando que g tem como valor a função dobro. Para comprovar isso de outra forma,
 # podemos fazer g.__name__, o que retornará 'dobro'
+
+# Sobre as funções anônimas
+# Em termos de performance, elas são exatamente iguais às funções nomeadas, algo que pode ser comprovado ao
+# executarmos um dis na função dobro2 acima.
+# A diferença primordial é que a função nomeada possui um __name__ específico, enquanto qualquer função anônima irá
+# retornar um mesmo valor para __name__: <lambda>.
+# Isso pode fazer com que seja difícil debugar algumas coisas. Tanto é que o Luciano Ramalho recomenda não utilizar
+# lambda no livro dele. Isso também é recomendado no PEP8, como podemos ver no código acima
